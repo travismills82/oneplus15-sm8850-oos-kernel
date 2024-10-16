353cc5a52ca4c387522c892898ef61400fb687ae
Fei <xuefei7@xiaomi.com>
2024-10-16 20:03:21 +0800

    ANDROID: module: Add vendor hooks
    Add vendor hook for module init, so we can get memory type and addr
    info, then we can use this info to set corresponding memory access
    atrribute in EL2 stage2 page table. We can get enhanced security
    protection by this way, as long as the stage2 page table is not
    corrupted.
    For releasing modules, corresponding page table attributes should be
    destroyed and restored.
    
    Bug: 441537639
    Change-Id: Ieccb3bdd1041dfe41a9c808a91cc19f04389e826
    Signed-off-by: Fei <xuefei7@xiaomi.com>
    (cherry picked from commit 6bc6743e622d4e615f120d5e363922c608501d87)
    (cherry picked from commit 71c3c86ad2272c1405675d114f29b7bfabc833ac)

--- kernel_platform/common/drivers/android/vendor_hooks.c
+++ kernel_platform/common/drivers/android/vendor_hooks.c
@@ -36,6 +36,7 @@
 #include <trace/hooks/health_report.h>
 #include <trace/hooks/avc.h>
 #include <trace/hooks/creds.h>
+#include <trace/hooks/module.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/syscall_check.h>
 #include <trace/hooks/gic.h>
@@ -223,6 +224,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_record_rtmutex_lock_starttime);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_record_rwsem_lock_starttime);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_record_pcpu_rwsem_starttime);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_wq_add);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_mod_mem);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_mod_perm_after_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_mod_perm_before_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_commit_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_exit_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_override_creds);
