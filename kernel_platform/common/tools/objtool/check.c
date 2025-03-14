f6b27ed672dd48adf02d5591648ba9c528a7ec5b
Josh Poimboeuf <jpoimboe@kernel.org>
2025-03-14 12:29:00 -0700

    objtool: Fix error handling inconsistencies in check()
    [ Upstream commit b745962cb97569aad026806bb0740663cf813147 ]
    
    Make sure all fatal errors are funneled through the 'out' label with a
    negative ret.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Brendan Jackman <jackmanb@google.com>
    Link: https://lore.kernel.org/r/0f49d6a27a080b4012e84e6df1e23097f44cc082.1741975349.git.jpoimboe@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel_platform/common/tools/objtool/check.c
+++ kernel_platform/common/tools/objtool/check.c
@@ -4759,8 +4759,10 @@ int check(struct objtool_file *file)
 	init_cfi_state(&force_undefined_cfi);
 	force_undefined_cfi.force_undefined = true;
 
-	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3)))
+	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3))) {
+		ret = -1;
 		goto out;
+	}
 
 	cfi_hash_add(&init_cfi);
 	cfi_hash_add(&func_cfi);
@@ -4777,7 +4779,7 @@ int check(struct objtool_file *file)
 	if (opts.retpoline) {
 		ret = validate_retpoline(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
@@ -4813,7 +4815,7 @@ int check(struct objtool_file *file)
 		 */
 		ret = validate_unrets(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
@@ -4876,7 +4878,7 @@ int check(struct objtool_file *file)
 	if (opts.prefix) {
 		ret = add_prefix_symbols(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
