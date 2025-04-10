0071ec1f271121452143cdf4c961e378256730e7
Chao Yu <chao@kernel.org>
2025-04-10 11:10:19 +0800

    f2fs: zone: fix to calculate first_zoned_segno correctly
    [ Upstream commit dc6d9ef57fcf42fac1b3be4bff5ac5b3f1e8f9f3 ]
    
    A zoned device can has both conventional zones and sequential zones,
    so we should not treat first segment of zoned device as first_zoned_segno,
    instead, we need to check zone type for each zone during traversing zoned
    device to find first_zoned_segno.
    
    Otherwise, for below case, first_zoned_segno will be 0, which could be
    wrong.
    
    create_null_blk 512 2 1024 1024
    mkfs.f2fs -m /dev/nullb0
    
    Testcase:
    
    export SCRIPTS_PATH=/share/git/scripts
    
    test multiple devices w/ zoned device
    for ((i=0;i<8;i++)) do {
    	zonesize=$((2<<$i))
    	conzone=$((4096/$zonesize))
    	seqzone=$((4096/$zonesize))
    	$SCRIPTS_PATH/nullblk_create.sh 512 $zonesize $conzone $seqzone
    	mkfs.f2fs -f -m /dev/vdb -c /dev/nullb0
    	mount /dev/vdb /mnt/f2fs
    	touch /mnt/f2fs/file
    	f2fs_io pinfile set /mnt/f2fs/file $((8589934592*2))
    	stat /mnt/f2fs/file
    	df
    	cat /proc/fs/f2fs/vdb/segment_info
    	umount /mnt/f2fs
    	$SCRIPTS_PATH/nullblk_remove.sh 0
    } done
    
    test single zoned device
    for ((i=0;i<8;i++)) do {
    	zonesize=$((2<<$i))
    	conzone=$((4096/$zonesize))
    	seqzone=$((4096/$zonesize))
    	$SCRIPTS_PATH/nullblk_create.sh 512 $zonesize $conzone $seqzone
    	mkfs.f2fs -f -m /dev/nullb0
    	mount /dev/nullb0 /mnt/f2fs
    	touch /mnt/f2fs/file
    	f2fs_io pinfile set /mnt/f2fs/file $((8589934592*2))
    	stat /mnt/f2fs/file
    	df
    	cat /proc/fs/f2fs/nullb0/segment_info
    	umount /mnt/f2fs
    	$SCRIPTS_PATH/nullblk_remove.sh 0
    } done
    
    Fixes: 9703d69d9d15 ("f2fs: support file pinning for zoned devices")
    Cc: Daeho Jeong <daehojeong@google.com>
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel_platform/common/fs/f2fs/data.c
+++ kernel_platform/common/fs/f2fs/data.c
@@ -4056,7 +4056,7 @@ retry:
 		if (!last_extent &&
 			((pblock - SM_I(sbi)->main_blkaddr) % blks_per_sec ||
 			nr_pblocks % blks_per_sec ||
-			!f2fs_valid_pinned_area(sbi, pblock))) {
+			f2fs_is_sequential_zone_area(sbi, pblock))) {
 			not_aligned++;
 
 			nr_pblocks = roundup(nr_pblocks, blks_per_sec);
