a58a9d8de613a0367063ec0e2265276ab0644042
Peter Zijlstra <peterz@infradead.org>
2025-05-20 11:19:30 +0200

    BACKPORT: UPSTREAM: sched/deadline: Less agressive dl_server handling
    Chris reported that commit 5f6bd380c7bd ("sched/rt: Remove default
    bandwidth control") caused a significant dip in his favourite
    benchmark of the day. Simply disabling dl_server cured things.
    
    His workload hammers the 0->1, 1->0 transitions, and the
    dl_server_{start,stop}() overhead kills it -- fairly obviously a bad
    idea in hind sight and all that.
    
    Change things around to only disable the dl_server when there has not
    been a fair task around for a whole period. Since the default period
    is 1 second, this ensures the benchmark never trips this, overhead
    gone.
    
    Fixes: 557a6bfc662c ("sched/fair: Add trivial fair server")
    Reported-by: Chris Mason <clm@meta.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Juri Lelli <juri.lelli@redhat.com>
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Link: https://lkml.kernel.org/r/20250702121158.465086194@infradead.org
    (cherry picked from commit cccb45d7c4295bbfeba616582d0249f2d21e6df5)
    [jstultz: Minor conflict resolution in removed lines in fair.c]
    Signed-off-by: John Stultz <jstultz@google.com>
    Change-Id: Id74a98f32324c866c764cc3eadfdc25c928cefb7
    (cherry picked from commit 849a7734d0e2d2dbbb9ec2ed812d575ecacfdf0d)

--- kernel_platform/common/kernel/sched/fair.c
+++ kernel_platform/common/kernel/sched/fair.c
@@ -6077,7 +6077,6 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
 	struct sched_entity *se;
 	long task_delta, idle_task_delta, delayed_delta, dequeue = 1;
-	long rq_h_nr_running = rq->cfs.h_nr_running;
 
 	raw_spin_lock(&cfs_b->lock);
 	/* This will start the period timer if necessary */
@@ -6162,9 +6161,6 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 	/* At this point se is NULL and we are at root level*/
 	sub_nr_running(rq, task_delta);
 
-	/* Stop the fair server if throttling resulted in no runnable tasks */
-	if (rq_h_nr_running && !rq->cfs.h_nr_running)
-		dl_server_stop(&rq->fair_server);
 done:
 	/*
 	 * Note: distribution will already see us throttled via the
@@ -7263,7 +7259,6 @@ static void set_next_buddy(struct sched_entity *se);
 static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 {
 	bool was_sched_idle = sched_idle_rq(rq);
-	int rq_h_nr_running = rq->cfs.h_nr_running;
 	bool task_sleep = flags & DEQUEUE_SLEEP;
 	bool task_delayed = flags & DEQUEUE_DELAYED;
 	struct task_struct *p = NULL;
@@ -7347,9 +7342,6 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 	}
 
 	sub_nr_running(rq, h_nr_running);
-
-	if (rq_h_nr_running && !rq->cfs.h_nr_running)
-		dl_server_stop(&rq->fair_server);
 
 	/* balance early to pull high priority tasks */
 	if (unlikely(!was_sched_idle && sched_idle_rq(rq)))
