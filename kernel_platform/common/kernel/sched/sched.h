4297359d8ce409b51ca8db9c858b87b3c8828fc5
Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
2025-07-09 17:33:28 +0000

    sched: Change nr_uninterruptible type to unsigned long
    commit 36569780b0d64de283f9d6c2195fd1a43e221ee8 upstream.
    
    The commit e6fe3f422be1 ("sched: Make multiple runqueue task counters
    32-bit") changed nr_uninterruptible to an unsigned int. But the
    nr_uninterruptible values for each of the CPU runqueues can grow to
    large numbers, sometimes exceeding INT_MAX. This is valid, if, over
    time, a large number of tasks are migrated off of one CPU after going
    into an uninterruptible state. Only the sum of all nr_interruptible
    values across all CPUs yields the correct result, as explained in a
    comment in kernel/sched/loadavg.c.
    
    Change the type of nr_uninterruptible back to unsigned long to prevent
    overflows, and thus the miscalculation of load average.
    
    Fixes: e6fe3f422be1 ("sched: Make multiple runqueue task counters 32-bit")
    
    Signed-off-by: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20250709173328.606794-1-aruna.ramakrishna@oracle.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel_platform/common/kernel/sched/sched.h
+++ kernel_platform/common/kernel/sched/sched.h
@@ -1180,7 +1180,7 @@ struct rq {
 	 * one CPU and if it got migrated afterwards it may decrease
 	 * it on another CPU. Always updated under the runqueue lock:
 	 */
-	unsigned int		nr_uninterruptible;
+	unsigned long 		nr_uninterruptible;
 
 #ifdef CONFIG_SCHED_PROXY_EXEC
 	struct task_struct __rcu	*donor;  /* Scheduling context */
