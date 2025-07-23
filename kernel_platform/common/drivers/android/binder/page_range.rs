d948711713a7c57f52ac79cd982ea1123a7db53f
Alice Ryhl <aliceryhl@google.com>
2025-07-23 12:43:49 +0000

    ANDROID: rust_binder: allocate pages array with kvmalloc
    Avoid making order 3 allocations with kmalloc. Using kvmalloc will help
    avoid OOM conditions.
    
    Bug: 350907206
    Change-Id: Ib1e8c113ac2cbe6b46938e2541ff76efc1f9d111
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    (cherry picked from commit 138082a93991ce2728a30a5426e23490ddeb4b77)

--- kernel_platform/common/drivers/android/binder/page_range.rs
+++ kernel_platform/common/drivers/android/binder/page_range.rs
@@ -21,7 +21,7 @@ use core::{
 };
 
 use kernel::{
-    alloc::allocator::Kmalloc,
+    alloc::allocator::KVmalloc,
     alloc::Allocator,
     bindings,
     error::Result,
@@ -296,7 +296,7 @@ impl ShrinkablePageRange {
 
         let layout = Layout::array::<PageInfo>(num_pages).map_err(|_| ENOMEM)?;
         // SAFETY: The layout has non-zero size.
-        let pages = Kmalloc::alloc(layout, GFP_KERNEL)?.cast::<PageInfo>();
+        let pages = KVmalloc::alloc(layout, GFP_KERNEL)?.cast::<PageInfo>();
 
         // SAFETY: This just initializes the pages array.
         unsafe {
@@ -316,7 +316,7 @@ impl ShrinkablePageRange {
             pr_debug!("Failed to register with vma: already registered");
             drop(inner);
             // SAFETY: The `pages` array was allocated with the same layout.
-            unsafe { Kmalloc::free(pages.cast(), layout) };
+            unsafe { KVmalloc::free(pages.cast(), layout) };
             return Err(EBUSY);
         }
 
@@ -646,7 +646,7 @@ impl PinnedDrop for ShrinkablePageRange {
         let layout = unsafe { Layout::array::<PageInfo>(size).unwrap_unchecked() };
 
         // SAFETY: The `pages` array was allocated with the same layout.
-        unsafe { Kmalloc::free(pages.cast(), layout) };
+        unsafe { KVmalloc::free(pages.cast(), layout) };
     }
 }
 
