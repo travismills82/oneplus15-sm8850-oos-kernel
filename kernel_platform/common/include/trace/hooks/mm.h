5f82662b2aabaf3c55fe969a4ba6fa03c996d0e5
Dongdong zhang <zhangdongdong5@xiaomi.com>
2025-07-24 20:07:06 +0800

    ANDROID: mm: add vendor hook to trace swap bio charge
    Add a vendor hook `android_vh_swap_bio_charge` in swap read path
    to allow vendors to monitor  behavior when a swap bio
    is submitted.
    
    Bug: 433890875
    
    Change-Id: I742978c7cb5c7d678d585b49e3466920f99e4fba
    Signed-off-by: Dongdong zhang <zhangdongdong5@xiaomi.com>
    (cherry picked from commit ee117211e482e1ed39390468ea91c7f2d05d2cca)

--- kernel_platform/common/include/trace/hooks/mm.h
+++ kernel_platform/common/include/trace/hooks/mm.h
@@ -613,6 +613,9 @@ DECLARE_HOOK(android_vh_folio_remove_rmap_ptes,
 DECLARE_HOOK(android_vh_gcma_cc_store_page_bypass,
 	TP_PROTO(bool *bypass),
 	TP_ARGS(bypass));
+DECLARE_HOOK(android_vh_swap_bio_charge,
+	TP_PROTO(struct bio *bio),
+	TP_ARGS(bio));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
