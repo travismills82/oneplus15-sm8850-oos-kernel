6059f28b995dbf3a54085e91f7fafc9a0f9c6170
kuyo chang <kuyo.chang@mediatek.corp-partner.google.com>
2025-08-08 11:30:31 +0800

    ANDROID: sched: Add trace hook for DL server
    Create a trace hook when the DL server is activated.
    This allows vendors to debug RT/Fair contention issues
    by dumping the following information:
    
    1.The task served by the DL server
    2.The status of the DL server
    
    Bug: 436885337
    
    Change-Id: I2157bd22b7d7c80d7ec1eeeba3ec6fb88efa4abd
    Signed-off-by: kuyo chang <kuyo.chang@mediatek.com>
    Signed-off-by: kuyo chang <kuyo.chang@mediatek.corp-partner.google.com>
    (cherry picked from commit 47cca682008eb27f897472f4ace70e1874e6fe44)

--- kernel_platform/common/include/trace/hooks/sched.h
+++ kernel_platform/common/include/trace/hooks/sched.h
@@ -515,6 +515,30 @@ DECLARE_HOOK(android_vh_prio_restore,
 	TP_PROTO(int saved_prio),
 	TP_ARGS(saved_prio));
 
+DECLARE_RESTRICTED_HOOK(android_rvh_update_rt_rq_load_avg,
+	TP_PROTO(u64 now, struct rq *rq, struct task_struct *tsk, int running),
+	TP_ARGS(now, rq, tsk, running), 1);
+
+struct sched_attr;
+
+DECLARE_HOOK(android_vh_set_sugov_sched_attr,
+	TP_PROTO(struct sched_attr *attr),
+	TP_ARGS(attr));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_set_iowait,
+	TP_PROTO(struct task_struct *p, struct rq *rq, int *should_iowait_boost),
+	TP_ARGS(p, rq, should_iowait_boost), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_util_fits_cpu,
+	TP_PROTO(unsigned long util, unsigned long uclamp_min, unsigned long uclamp_max,
+		 int cpu, bool *fits, bool *done),
+	TP_ARGS(util, uclamp_min, uclamp_max, cpu, fits, done), 1);
+
+struct sched_dl_entity;
+DECLARE_HOOK(android_vh_dump_dl_server,
+	TP_PROTO(struct sched_dl_entity *dl_se, struct task_struct *p),
+	TP_ARGS(dl_se, p));
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
