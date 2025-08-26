a5177fed7c6e55549b3fd31603bfb652b9c3593a
Sebastian Ene <sebastianene@google.com>
2025-08-26 13:00:32 +0000

    ANDROID: KVM: Prevent memory sharing outside of the RAM regions
    Verify the address to be shared and prevent MMIO page
    sharing. Allow MMIO to be donated to the hypervisor and
    to the guest for device assignment.
    
    Bug: 439996285
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    
    Cleaned up host_check_page_state_flags.
    
    Change-Id: I98590534865d3239911350736831b8564d614fef
    Signed-off-by: Keir Fraser <keirf@google.com>
    (cherry picked from commit 33eb6bde43d03bd826214bbb390de62ca19621b9)

--- kernel_platform/common/arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ kernel_platform/common/arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -995,10 +995,6 @@ static int ___host_check_page_state_range(u64 addr, u64 size,
 	struct hyp_page *p;
 	struct memblock_region *reg;
 	struct kvm_mem_range range;
-	u64 end;
-
-	if (check_add_overflow(addr, size, &end))
-		return -EINVAL;
 
 	/* Can't check the state of both MMIO and memory regions at once */
 	reg = find_mem_range(addr, &range);
@@ -1630,7 +1626,7 @@ int __pkvm_hyp_donate_host(u64 pfn, u64 nr_pages)
 	ret = __hyp_check_page_state_range(virt, size, PKVM_PAGE_OWNED);
 	if (ret)
 		goto unlock;
-	if (IS_ENABLED(CONFIG_NVHE_EL2_DEBUG)) {
+	if (IS_ENABLED(CONFIG_PKVM_STRICT_CHECKS)) {
 		ret = ___host_check_page_state_range(phys, size, PKVM_NOPAGE, 0);
 		if (ret)
 			goto unlock;
@@ -1641,6 +1637,48 @@ int __pkvm_hyp_donate_host(u64 pfn, u64 nr_pages)
 
 unlock:
 	hyp_unlock_component();
+	host_unlock_component();
+	return ret;
+}
+
+int __pkvm_host_donate_ffa(u64 pfn, u64 nr_pages)
+{
+	u64 size, phys = hyp_pfn_to_phys(pfn), end;
+	int ret;
+
+	if (check_shl_overflow(nr_pages, PAGE_SHIFT, &size) ||
+	    check_add_overflow(phys, size, &end))
+		return -EINVAL;
+
+	host_lock_component();
+
+	ret = __host_check_page_state_range(phys, size, PKVM_PAGE_OWNED);
+	if (ret)
+		goto unlock;
+
+	WARN_ON(host_stage2_set_owner_locked(phys, size, PKVM_ID_FFA));
+unlock:
+	host_unlock_component();
+	return ret;
+}
+
+int __pkvm_host_reclaim_ffa(u64 pfn, u64 nr_pages)
+{
+	u64 size, phys = hyp_pfn_to_phys(pfn), end;
+	int ret;
+
+	if (check_shl_overflow(nr_pages, PAGE_SHIFT, &size) ||
+	    check_add_overflow(phys, size, &end))
+		return -EINVAL;
+
+	host_lock_component();
+
+	ret = __host_check_page_state_range(phys, size, PKVM_NOPAGE);
+	if (ret)
+		goto unlock;
+
+	WARN_ON(host_stage2_set_owner_locked(phys, size, PKVM_ID_HOST));
+unlock:
 	host_unlock_component();
 	return ret;
 }
@@ -1895,16 +1933,21 @@ static int __pkvm_use_dma_locked(phys_addr_t phys_addr, size_t size,
 		if (hyp_vcpu)
 			return EINVAL;
 
-		ret = ___host_check_page_state_range(phys_addr, size,
-						     PKVM_PAGE_TAINTED,
-						     0);
-		if (!ret)
-			return ret;
-		ret = ___host_check_page_state_range(phys_addr, size,
-						     PKVM_PAGE_OWNED,
-						     0);
-		if (ret)
-			return ret;
+		for (i = 0; i < nr_pages; i++) {
+			u64 addr = phys_addr + i * PAGE_SIZE;
+
+			ret = ___host_check_page_state_range(addr, PAGE_SIZE,
+							     PKVM_PAGE_TAINTED,
+							     0);
+			/* Page already tainted */
+			if (!ret)
+				continue;
+			ret = ___host_check_page_state_range(addr, PAGE_SIZE,
+							     PKVM_PAGE_OWNED,
+							     0);
+			if (ret)
+				return ret;
+		}
 		prot = pkvm_mkstate(PKVM_HOST_MMIO_PROT, PKVM_PAGE_TAINTED);
 		ret = host_stage2_idmap_locked(phys_addr, size, prot, false);
 	} else {
