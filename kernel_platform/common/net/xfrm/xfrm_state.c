229a8b0f9236789a82c8a53976827e5ed7231017
Sabrina Dubroca <sd@queasysnail.net>
2025-08-29 10:54:15 +0200

    xfrm: xfrm_alloc_spi shouldn't use 0 as SPI
    [ Upstream commit cd8ae32e4e4652db55bce6b9c79267d8946765a9 ]
    
    x->id.spi == 0 means "no SPI assigned", but since commit
    94f39804d891 ("xfrm: Duplicate SPI Handling"), we now create states
    and add them to the byspi list with this value.
    
    __xfrm_state_delete doesn't remove those states from the byspi list,
    since they shouldn't be there, and this shows up as a UAF the next
    time we go through the byspi list.
    
    Reported-by: syzbot+a25ee9d20d31e483ba7b@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=a25ee9d20d31e483ba7b
    Fixes: 94f39804d891 ("xfrm: Duplicate SPI Handling")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 9fcedabaae0096f712bbb4ccca6a8538af1cd1c8)

--- kernel_platform/common/net/xfrm/xfrm_state.c
+++ kernel_platform/common/net/xfrm/xfrm_state.c
@@ -2492,11 +2492,20 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high,
 
 	err = -ENOENT;
 
-	if (minspi == maxspi) {
-		x0 = xfrm_state_lookup(net, mark, &x->id.daddr, minspi, x->id.proto, x->props.family);
-		if (x0) {
-			NL_SET_ERR_MSG(extack, "Requested SPI is already in use");
-			xfrm_state_put(x0);
+	for (h = 0; h < range; h++) {
+		u32 spi = (low == high) ? low : get_random_u32_inclusive(low, high);
+		if (spi == 0)
+			goto next;
+		newspi = htonl(spi);
+
+		spin_lock_bh(&net->xfrm.xfrm_state_lock);
+		x0 = xfrm_state_lookup_spi_proto(net, newspi, x->id.proto);
+		if (!x0) {
+			x->id.spi = newspi;
+			h = xfrm_spi_hash(net, &x->id.daddr, newspi, x->id.proto, x->props.family);
+			XFRM_STATE_INSERT(byspi, &x->byspi, net->xfrm.state_byspi + h, x->xso.type);
+			spin_unlock_bh(&net->xfrm.xfrm_state_lock);
+			err = 0;
 			goto unlock;
 		}
 		newspi = minspi;
@@ -2520,8 +2529,17 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high,
 				  x->xso.type);
 		spin_unlock_bh(&net->xfrm.xfrm_state_lock);
 
-		err = 0;
-	} else {
+next:
+		if (signal_pending(current)) {
+			err = -ERESTARTSYS;
+			goto unlock;
+		}
+
+		if (low == high)
+			break;
+	}
+
+	if (err)
 		NL_SET_ERR_MSG(extack, "No SPI available in the requested range");
 	}
 
