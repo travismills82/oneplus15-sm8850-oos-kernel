8e3bbf602e0d054f02b57aafc1ef04ebc4e1bdf6
panfan <panfan@qti.qualcomm.com>
2025-09-04 20:22:36 -0700

    arm64: ftrace: fix unreachable PLT for ftrace_caller in init_module with CONFIG_DYNAMIC_FTRACE
    commit a7ed7b9d0ebb038db9963d574da0311cab0b666a upstream.
    
    On arm64, it has been possible for a module's sections to be placed more
    than 128M away from each other since commit:
    
      commit 3e35d303ab7d ("arm64: module: rework module VA range selection")
    
    Due to this, an ftrace callsite in a module's .init.text section can be
    out of branch range for the module's ftrace PLT entry (in the module's
    .text section). Any attempt to enable tracing of that callsite will
    result in a BRK being patched into the callsite, resulting in a fatal
    exception when the callsite is later executed.
    
    Fix this by adding an additional trampoline for .init.text, which will
    be within range.
    
    No additional trampolines are necessary due to the way a given
    module's executable sections are packed together. Any executable
    section beginning with ".init" will be placed in MOD_INIT_TEXT,
    and any other executable section, including those beginning with ".exit",
     will be placed in MOD_TEXT.
    
    Fixes: 3e35d303ab7d ("arm64: module: rework module VA range selection")
    Cc: <stable@vger.kernel.org> # 6.5.x
    Signed-off-by: panfan <panfan@qti.qualcomm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20250905032236.3220885-1-panfan@qti.qualcomm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 650c14abe3031dd222e8c4d042cf49317ff26770)

--- kernel_platform/common/arch/arm64/include/asm/module.h
+++ kernel_platform/common/arch/arm64/include/asm/module.h
@@ -65,8 +65,7 @@ struct mod_arch_specific {
 
 	/* for CONFIG_DYNAMIC_FTRACE */
 	struct plt_entry	*ftrace_trampolines;
-
-	ARM64_MODULE_KVM_ARCHDATA
+	struct plt_entry	*init_ftrace_trampolines;
 };
 
 u64 module_emit_plt_entry(struct module *mod, Elf64_Shdr *sechdrs,
