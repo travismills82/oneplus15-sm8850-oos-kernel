5ba3bf73124f4f949d9580af3855ba783654008d
Greg Kroah-Hartman <gregkh@google.com>
2025-11-20 11:03:32 +0000

    ANDROID: KABI: fix up abi change in struct usb_request
    In commit 1a3949c3e5c3 ("usb: gadget: Store endpoint pointer in
    usb_request"), a new pointer is added to struct usb_request to fix some
    issues with usb gadget drivers.  As we want those fixes in the
    android14-6.1 tree, use a reserved kabi field for this new pointer to
    preserve the abi.
    
    Fixes: 1a3949c3e5c3 ("usb: gadget: Store endpoint pointer in usb_request")
    Change-Id: Ieacf1186052a20c43442576e6d8695e86a2189ed
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel_platform/common/include/linux/usb/gadget.h
+++ kernel_platform/common/include/linux/usb/gadget.h
@@ -100,7 +100,6 @@ struct usb_ep;
  */
 
 struct usb_request {
-	struct usb_ep		*ep;
 	void			*buf;
 	unsigned		length;
 	dma_addr_t		dma;
@@ -127,7 +126,7 @@ struct usb_request {
 	int			status;
 	unsigned		actual;
 
-	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_USE(1, struct usb_ep *ep);
 };
 
 /*-------------------------------------------------------------------------*/
