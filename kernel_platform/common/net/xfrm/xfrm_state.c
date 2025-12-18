e1116da6a957c5200f65c0f8362dcfc42d909846
Greg Kroah-Hartman <gregkh@google.com>
2025-12-18 16:05:28 +0000

    Revert "xfrm: delete x->tunnel as we delete x"
    This reverts commit dc3636912d41770466543623cb76e7b88fdb42c7 which is
    commit b441cf3f8c4b8576639d20c8eb4aa32917602ecd upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Iab5618cd8be9cede8b4491245617af9ca19e0804
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel_platform/common/net/xfrm/xfrm_state.c
+++ kernel_platform/common/net/xfrm/xfrm_state.c
@@ -748,7 +748,6 @@ void __xfrm_state_destroy(struct xfrm_state *x, bool sync)
 }
 EXPORT_SYMBOL(__xfrm_state_destroy);
 
-static void xfrm_state_delete_tunnel(struct xfrm_state *x);
 int __xfrm_state_delete(struct xfrm_state *x)
 {
 	struct net *net = xs_net(x);
@@ -778,8 +777,6 @@ int __xfrm_state_delete(struct xfrm_state *x)
 			sock_put(rcu_dereference_raw(x->encap_sk));
 
 		xfrm_dev_state_delete(x);
-
-		xfrm_state_delete_tunnel(x);
 
 		/* All xfrm_state objects are created by xfrm_state_alloc.
 		 * The xfrm_state_alloc call gives a reference, and that
@@ -884,7 +881,10 @@ restart:
 				err = xfrm_state_delete(x);
 				xfrm_audit_state_delete(x, err ? 0 : 1,
 							task_valid);
-				xfrm_state_put(x);
+				if (sync)
+					xfrm_state_put_sync(x);
+				else
+					xfrm_state_put(x);
 				if (!err)
 					cnt++;
 
@@ -3003,17 +3003,20 @@ void xfrm_flush_gc(void)
 }
 EXPORT_SYMBOL(xfrm_flush_gc);
 
-static void xfrm_state_delete_tunnel(struct xfrm_state *x)
+/* Temporarily located here until net/xfrm/xfrm_tunnel.c is created */
+void xfrm_state_delete_tunnel(struct xfrm_state *x)
 {
 	if (x->tunnel) {
 		struct xfrm_state *t = x->tunnel;
 
-		if (atomic_dec_return(&t->tunnel_users) == 1)
+		if (atomic_read(&t->tunnel_users) == 2)
 			xfrm_state_delete(t);
-		xfrm_state_put(t);
+		atomic_dec(&t->tunnel_users);
+		xfrm_state_put_sync(t);
 		x->tunnel = NULL;
 	}
 }
+EXPORT_SYMBOL(xfrm_state_delete_tunnel);
 
 u32 xfrm_state_mtu(struct xfrm_state *x, int mtu)
 {
@@ -3214,8 +3217,8 @@ void xfrm_state_fini(struct net *net)
 	unsigned int sz;
 
 	flush_work(&net->xfrm.state_hash_work);
-	xfrm_state_flush(net, 0, false, true);
 	flush_work(&xfrm_state_gc_work);
+	xfrm_state_flush(net, 0, false, true);
 
 	WARN_ON(!list_empty(&net->xfrm.state_all));
 
