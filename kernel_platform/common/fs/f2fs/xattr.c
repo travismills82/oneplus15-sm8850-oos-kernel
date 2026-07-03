7a4db1e6f44fc5141e17e19d8d9c4562751252e3
Keshav Verma <iganschel@gmail.com>
2026-07-03 14:50:15 -0400

    f2fs: fix listxattr handling of corrupted xattr entries
    [ Upstream commit 5ef5bc304f23c3fe255d4936472378dcb74d0e94 ]
    
    Validate the xattr entry before reading its fields in f2fs_listxattr().
    Return -EFSCORRUPTED when the entry is outside the valid xattr storage
    area instead of returning a successful partial result.
    
    Fixes: 688078e7f36c ("f2fs: fix to avoid memory leakage in f2fs_listxattr")
    Cc: stable@kernel.org
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Keshav Verma <iganschel@gmail.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7dd01f7d0291583e3e5420c95c7d584e114899bd)

--- kernel_platform/common/fs/f2fs/xattr.c
+++ kernel_platform/common/fs/f2fs/xattr.c
@@ -580,8 +580,6 @@ ssize_t f2fs_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size)
 		size_t prefix_len;
 		size_t size;
 
-		prefix = f2fs_xattr_prefix(entry->e_name_index, dentry);
-
 		if ((void *)(entry) + sizeof(__u32) > last_base_addr ||
 			(void *)XATTR_NEXT_ENTRY(entry) > last_base_addr) {
 			f2fs_err(F2FS_I_SB(inode), "list inode (%lu) has corrupted xattr",
@@ -589,9 +587,11 @@ ssize_t f2fs_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size)
 			set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_FSCK);
 			f2fs_handle_error(F2FS_I_SB(inode),
 						ERROR_CORRUPTED_XATTR);
-			break;
+			error = -EFSCORRUPTED;
+			goto cleanup;
 		}
 
+		prefix = f2fs_xattr_prefix(entry->e_name_index, dentry);
 		if (!prefix)
 			continue;
 
