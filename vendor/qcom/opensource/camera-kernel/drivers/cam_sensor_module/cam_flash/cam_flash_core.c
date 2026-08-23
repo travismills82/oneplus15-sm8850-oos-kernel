ca405a58e9d5fd5967db4b2509f2a37b6ba9f2a9
travismills82@gmail.com <travismills82@gmail.com>
2026-08-23 14:28:48 -0500

    camera: snapshot RER command before validation

--- vendor/qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ vendor/qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -1652,6 +1652,7 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 	struct cam_req_mgr_add_request add_req = {0};
 	struct cam_flash_init *cam_flash_info = NULL;
 	struct cam_flash_set_rer *flash_rer_info = NULL;
+	struct cam_flash_set_rer *flash_rer_info_u = NULL;
 	struct cam_flash_set_on_off *flash_operation_info = NULL;
 	struct cam_flash_set_on_off *flash_operation_info_u = NULL;
 	struct cam_flash_query_curr *flash_query_info = NULL;
@@ -2072,17 +2073,38 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 				rc = -EINVAL;
 				break;
 			}
-			flash_rer_info = (struct cam_flash_set_rer *)cmd_buf;
-			if (!flash_rer_info) {
+			flash_rer_info_u = (struct cam_flash_set_rer *)cmd_buf;
+			if (!flash_rer_info_u) {
 				CAM_ERR(CAM_FLASH,
 					"flash_rer_info Null");
 				rc = -EINVAL;
 				break;
 			}
+
+			count = flash_rer_info_u->count;
+			rc = cam_common_mem_kdup((void **)&flash_rer_info,
+				flash_rer_info_u,
+				sizeof(struct cam_flash_set_rer));
+			if (rc) {
+				CAM_ERR(CAM_FLASH,
+					"Alloc and copy flash RER info failed");
+				break;
+			}
+
+			if (count != flash_rer_info->count) {
+				CAM_ERR(CAM_FLASH,
+					"Count changed: userspace: %d, kernel: %d",
+					count, flash_rer_info->count);
+				rc = -EINVAL;
+				cam_common_mem_free(flash_rer_info);
+				break;
+			}
+
 			if (flash_rer_info->count >
 				CAM_FLASH_MAX_LED_TRIGGERS) {
 				CAM_ERR(CAM_FLASH, "led count out of limit");
 				rc = -EINVAL;
+				cam_common_mem_free(flash_rer_info);
 				break;
 			}
 
@@ -2106,6 +2128,7 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 			if (rc)
 				CAM_ERR(CAM_FLASH, "apply_setting failed: %d",
 					rc);
+			cam_common_mem_free(flash_rer_info);
 			break;
 		}
 		default:
