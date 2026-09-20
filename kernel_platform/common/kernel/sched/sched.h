b1c0872af25d12302c6d4b52443ef8a943d7e366
John Stultz <jstultz@google.com>
2026-09-20 09:35:49 -0500

    ANDROID: sched: preserve min_vruntime names for KMI5 compatibility
    Use the official Android android16-6.12 KABI representation to retain the qualified generation-5 struct cfs_rq layout and source names while stable code uses zero_vruntime.
    
    Android-commit: 65b6e4394fe517f93be222cf9370b21e51561881

--- kernel_platform/common/kernel/sched/sched.h
+++ kernel_platform/common/kernel/sched/sched.h
@@ -697,10 +697,18 @@ struct cfs_rq {
 	s64			avg_vruntime;
 	u64			avg_load;
 
-	u64			zero_vruntime;
+	ANDROID_KABI_REPLACE(u64, min_vruntime,
+		union {
+			u64			min_vruntime;
+			u64			zero_vruntime;
+		});
 #ifdef CONFIG_SCHED_CORE
 	unsigned int		forceidle_seq;
-	u64			zero_vruntime_fi;
+	ANDROID_KABI_REPLACE(u64, min_vruntime_fi,
+		union {
+			u64			min_vruntime_fi;
+			u64			zero_vruntime_fi;
+		});
 #endif
 
 	struct rb_root_cached	tasks_timeline;
